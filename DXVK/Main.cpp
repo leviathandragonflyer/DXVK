97298af472f28100663e2ca3a1bb8e061914f28d
Leviathan Dragonflyer <levidrago@outlook.com>
2015-08-10 19:18:50 -0400

    Initial Commit

---
+++ DXVK/Main.cpp
@@ -0,0 +1,12 @@
+#define DXVK_BIN L"DXVK.exe"
+
+#define WINDOWS_LEAN_AND_MEAN
+#include <iostream>
+
+#include <Windows.h>
+#include <Shellapi.h>
+
+int main(int argc, const char ** argv)
+{
+	return EXIT_SUCCESS;
+}
